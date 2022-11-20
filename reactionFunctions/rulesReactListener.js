0d46afbd622b4d2aabc23f9e3d949350f5018da1
Tyron <lucena.tyronscott@gmail.com>
2022-11-20 14:19:07 +0800

    feat: added +verify command

--- reactionFunctions/rulesReactListener.js
+++ reactionFunctions/rulesReactListener.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: "rulesReactListner",
-    description: "self explanatory",    
+    description: "self explanatory",
     async execute(reaction, user) {
         if (reaction.message.id != "1034815908650176513") {
             return;
@@ -12,7 +12,7 @@ module.exports = {
 
         const verifiedRole = await reaction.message.guild.roles.fetch("1043555475964309574") // verified role
         const lilliesRole = await reaction.message.guild.roles.fetch("1034678784135278662") // lillies role id
-        
+
         // find the member using its id
         reaction.message.guild.members.fetch(user.id).then(member => {
             // remove the verified role
