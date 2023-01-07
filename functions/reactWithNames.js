6c668e43d7fa2a8807133738843fbc737fb75de3
Tyron <lucena.tyronscott@gmail.com>
2023-01-07 23:02:40 +0800

    added custom react replies

--- functions/reactWithNames.js
+++ functions/reactWithNames.js
@@ -1,7 +1,81 @@
+const emojiMap = {
+    "a": "🇦",
+    "b": "🇧",
+    "c": "🇨",
+    "d": "🇩",
+    "e": "🇪",
+    "f": "🇫",
+    "g": "🇬",
+    "h": "🇭",
+    "i": "🇮",
+    "j": "🇯",
+    "k": "🇰",
+    "l": "🇱",
+    "m": "🇲",
+    "n": "🇳",
+    "o": "🇴",
+    "p": "🇵",
+    "q": "🇶",
+    "r": "🇷",
+    "s": "🇸",
+    "t": "🇹",
+    "u": "🇺",
+    "v": "🇻",
+    "w": "🇼",
+    "x": "🇽",
+    "y": "",
+    "z": "🇿",
+}
+
+const reactionMap = new Object();
+
 module.exports = {
     name: "reactWithNames",
     description: "Reacts with the name on every message that contains this name",
     async execute(message) {
+        if (message.content.startsWith(`+setReact`)) {
+            const args = message.content.slice('setReact'.length + 1).trim().split(/ +/);
+
+            console.log(args);
+
+            const key = args[0];
+            const reactMessage = args[1];
+
+            if (key && reactMessage) {
+                if (hasRepeats(reactMessage)) {
+                    message.reply("Reaction message cannot contain duplicate characters. (Discord does not allow the same emoji to appear more than once.");
+                    return;
+                }
+
+                var reactions = []
+
+                for (let i = 0; i < reactMessage.length; i++) {
+                    let currentLetter = reactMessage.charAt(i);
+
+                    let correspondingEmoji = emojiMap[currentLetter]
+                    if (correspondingEmoji == null) {
+                        message.reply("Reaction message must only contain A-Z characters.");
+                        return;
+                    }
+
+                    reactions.push(correspondingEmoji);
+                }
+
+                reactionMap[key] = reactions;
+
+                await message.reply("Reaction successfully set.");
+                return;
+            }
+        }
+
+        var toReact = reactionMap[message.content.toString()];
+        if (toReact != null) {
+            for (let i = 0; i < toReact.length; i++) {
+                await message.react(toReact[i])
+            }
+            return;
+        }
+
         if (message.content.toLowerCase().includes("tyron")) {
             message.react("🇹")
                 .then(() => message.react("🇾"))
@@ -30,4 +104,8 @@ module.exports = {
                 .catch(console.log);
         }
     }
+}
+
+function hasRepeats(str) {
+    return /(.).*\1/.test(str);
 }
